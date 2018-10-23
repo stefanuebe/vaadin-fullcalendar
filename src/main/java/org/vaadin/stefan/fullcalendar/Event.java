c0711e5b9029cc0da9c31b5a34444db2e1e13275
Stefan Uebe <stefan@vaadin.com>
2018-10-23 15:54:38 +0200

    added more constructors, impl hashcode and equals

--- src/main/java/org/vaadin/stefan/fullcalendar/Event.java
+++ src/main/java/org/vaadin/stefan/fullcalendar/Event.java
@@ -28,9 +28,23 @@ public class Event {
         this(null, title, start, end, false);
     }
 
+    public Event(String id, String title, LocalDate date) {
+        this(id, title, date.atStartOfDay(), null, true);
+    }
+
+    public Event(String id, String title, LocalDate start, LocalDate end) {
+        this(id, title, start.atStartOfDay(), end.atTime(23, 59, 59, 999), true);
+    }
+
+    public Event(String id, String title, LocalDateTime start, LocalDateTime end) {
+        this(id, title, start, end, false);
+    }
+
+
     private Event(String id, @Nonnull String title, @Nonnull LocalDateTime start, LocalDateTime end, boolean fullDayEvent) {
         Objects.requireNonNull(title);
         Objects.requireNonNull(start);
+
         this.id = id;
         this.title = title;
         this.start = start;
@@ -56,5 +70,26 @@ public class Event {
 
     public boolean isFullDayEvent() {
         return fullDayEvent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Event event = (Event) o;
+        return fullDayEvent == event.fullDayEvent &&
+                Objects.equals(id, event.id) &&
+                Objects.equals(title, event.title) &&
+                Objects.equals(start, event.start) &&
+                Objects.equals(end, event.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, start, end, fullDayEvent);
     }
 }
