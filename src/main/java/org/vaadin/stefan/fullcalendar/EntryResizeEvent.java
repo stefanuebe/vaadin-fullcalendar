e5e87236e51179bfd61fbe7da27314f36d162f32
Stefan Uebe <stefan@vaadin.com>
2018-10-24 09:54:57 +0200

    event extraction

--- src/main/java/org/vaadin/stefan/fullcalendar/EntryResizeEvent.java
+++ src/main/java/org/vaadin/stefan/fullcalendar/EntryResizeEvent.java
@@ -1,15 +1,11 @@
 package org.vaadin.stefan.fullcalendar;
 
-import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.EventData;
 import elemental.json.JsonObject;
 
 @DomEvent("eventResize")
-public class EntryResizeEvent extends ComponentEvent<FullCalendar> {
-
-    private final Entry entry;
-    private final Delta delta;
+public class EntryResizeEvent extends EntryDeltaEvent {
 
     /**
      * Creates a new event using the given source and indicator whether the
@@ -19,23 +15,10 @@ public class EntryResizeEvent extends ComponentEvent<FullCalendar> {
      * @param fromClient <code>true</code> if the event originated from the client
      */
     public EntryResizeEvent(FullCalendar source, boolean fromClient, @EventData("event.detail.id") String id, @EventData("event.detail.delta") JsonObject delta) {
-        super(source, fromClient);
-        this.entry = source.getEntryById(id).orElseThrow(IllegalArgumentException::new);
+        super(source, fromClient, id, delta);
 
-        this.delta = Delta.fromJson(delta);
-        entry.setEnd(this.delta.applyOn(entry.getEnd().orElseGet(entry::getStart)));
-    }
+        Entry entry = getEntry();
+        entry.setEnd(getDelta().applyOn(entry.getEnd().orElseGet(entry::getStart)));
 
-
-    /**
-     * Returns the modified event. The end date of this event has already been updated by the delta.
-     * @return event
-     */
-    public Entry getEntry() {
-        return entry;
-    }
-
-    public Delta getDelta() {
-        return delta;
     }
 }
