62e35d2646306c6a2c57155dd3441fb10189ef20
Stefan Uebe <stefan@vaadin.com>
2018-10-24 09:54:57 +0200

    event extraction

--- src/main/java/org/vaadin/stefan/fullcalendar/EntryClickEvent.java
+++ src/main/java/org/vaadin/stefan/fullcalendar/EntryClickEvent.java
@@ -5,9 +5,7 @@ import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.EventData;
 
 @DomEvent("eventClick")
-public class EntryClickEvent extends ComponentEvent<FullCalendar> {
-
-    private final Entry entry;
+public class EntryClickEvent extends EntryEvent {
 
     /**
      * Creates a new event using the given source and indicator whether the
@@ -17,11 +15,7 @@ public class EntryClickEvent extends ComponentEvent<FullCalendar> {
      * @param fromClient <code>true</code> if the event originated from the client
      */
     public EntryClickEvent(FullCalendar source, boolean fromClient, @EventData("event.detail.id") String id) {
-        super(source, fromClient);
-        this.entry = source.getEntryById(id).orElseThrow(IllegalArgumentException::new);
+        super(source, fromClient, id);
     }
 
-    public Entry getEntry() {
-        return entry;
-    }
 }
