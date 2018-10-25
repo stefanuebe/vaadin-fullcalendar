e17105cca489a48b6217306cfb86de9724209de1
Stefan Uebe <stefan@vaadin.com>
2018-10-25 09:33:18 +0200

    added comment to click event

--- src/main/java/org/vaadin/stefan/fullcalendar/DayClickEvent.java
+++ src/main/java/org/vaadin/stefan/fullcalendar/DayClickEvent.java
@@ -9,6 +9,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.Optional;
 
+/**
+ * This event occurs when an empty slot in the calendar has clicked. Empty means the empty space itself, not that there
+ * is not yet another entry located at this timeslot.
+ */
 @DomEvent("dayClick")
 public class DayClickEvent extends ComponentEvent<FullCalendar> {
 
@@ -16,11 +20,11 @@ public class DayClickEvent extends ComponentEvent<FullCalendar> {
     private LocalDate clickedDate;
 
     /**
-     * Creates a new event using the given source and indicator whether the
-     * event originated from the client side or the server side.
+     * New instance. Awaits the clicked date (time) as iso string (e.g. "2018-10-23" or "2018-10-23T13:30").
      *
      * @param source     the source component
      * @param fromClient <code>true</code> if the event originated from the client
+     * @param date clicked time slot as iso string
      */
     public DayClickEvent(FullCalendar source, boolean fromClient, @EventData("event.detail.date") String date) {
         super(source, fromClient);
