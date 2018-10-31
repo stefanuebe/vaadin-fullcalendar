bf6ec3dccc6ac938e8b71429bc705bd7d66015ea
Stefan Uebe <stefan@vaadin.com>
2018-10-31 11:46:09 +0100

    1.1.0 start

--- demo/src/main/java/org/vaadin/stefan/Demo.java
+++ demo/src/main/java/org/vaadin/stefan/Demo.java
@@ -41,7 +41,7 @@ public class Demo extends Div {
     private HorizontalLayout toolbar;
 
     public Demo() {
-        HorizontalLayout title = new HorizontalLayout(new H3("full calendar demo"), new Span("(1.0.0)"));
+        HorizontalLayout title = new HorizontalLayout(new H3("full calendar demo"), new Span("(1.1.0)"));
         title.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.BASELINE);
         add(title);
 
