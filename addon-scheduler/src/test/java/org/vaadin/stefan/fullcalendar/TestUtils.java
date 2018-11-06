d63e3f28c76de4ec5aff77b9912aeab2c780b3a5
Stefan Uebe <stefan@vaadin.com>
2018-11-06 11:45:37 +0100

    more tests

--- addon-scheduler/src/test/java/org/vaadin/stefan/fullcalendar/TestUtils.java
+++ addon-scheduler/src/test/java/org/vaadin/stefan/fullcalendar/TestUtils.java
@@ -20,36 +20,34 @@ public class TestUtils {
      * @param webComponentUrlParts relative paths of web component html files
      */
     public static void initVaadinService(final String... webComponentUrlParts) {
-        if (inited) {
-            throw new UnsupportedOperationException("Please call this method only once!");
-        }
+        if (!inited) {
 
-        VaadinService vaadinService = Mockito.mock(VaadinService.class);
-        VaadinService.setCurrent(vaadinService);
+            VaadinService vaadinService = Mockito.mock(VaadinService.class);
+            VaadinService.setCurrent(vaadinService);
 
-        Mockito.when(vaadinService.getDeploymentConfiguration())
-                .thenAnswer(invocation -> {
-                    DeploymentConfiguration config = Mockito.mock(DeploymentConfiguration.class);
-                    Mockito.when(config.isProductionMode()).thenReturn(false);
-                    return config;
-                });
+            Mockito.when(vaadinService.getDeploymentConfiguration())
+                    .thenAnswer(invocation -> {
+                        DeploymentConfiguration config = Mockito.mock(DeploymentConfiguration.class);
+                        Mockito.when(config.isProductionMode()).thenReturn(false);
+                        return config;
+                    });
 
-        if (webComponentUrlParts != null) {
-            for (String part : webComponentUrlParts) {
-                Mockito.when(vaadinService.
-                        getResourceAsStream(ArgumentMatchers.eq("frontend://bower_components/" + part), ArgumentMatchers.any(), ArgumentMatchers.any())).
-                        thenAnswer(invocation -> {
-                            Path path = Paths.get("src/main/resources/META-INF/resources/frontend/bower_components/" + part).toAbsolutePath();
-                            if (!Files.isRegularFile(path)) {
-                                path = Paths.get("src/test/resources/META-INF/resources/frontend/bower_components/" + part).toAbsolutePath();
-                            }
-                            return Files.newInputStream(path);
-                        });
+            if (webComponentUrlParts != null) {
+                for (String part : webComponentUrlParts) {
+                    Mockito.when(vaadinService.
+                            getResourceAsStream(ArgumentMatchers.eq("frontend://bower_components/" + part), ArgumentMatchers.any(), ArgumentMatchers.any())).
+                            thenAnswer(invocation -> {
+                                Path path = Paths.get("src/main/resources/META-INF/resources/frontend/bower_components/" + part).toAbsolutePath();
+                                if (!Files.isRegularFile(path)) {
+                                    path = Paths.get("src/test/resources/META-INF/resources/frontend/bower_components/" + part).toAbsolutePath();
+                                }
+                                return Files.newInputStream(path);
+                            });
 
+                }
             }
+
+            inited = true;
         }
-
-        inited = true;
-
     }
 }
