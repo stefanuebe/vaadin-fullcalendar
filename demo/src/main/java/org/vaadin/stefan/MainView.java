db958bea952c9c9adab0ade91354d946251bf09b
Stefan Uebe <stefan@vaadin.com>
2018-11-15 16:55:57 +0100

    added license, removed unneeded imports

--- demo/src/main/java/org/vaadin/stefan/MainView.java
+++ demo/src/main/java/org/vaadin/stefan/MainView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018, Stefan Uebe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package org.vaadin.stefan;
 
 import com.vaadin.flow.component.Tag;
@@ -17,7 +33,7 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 @Tag("main-view")
 @HtmlImport("main-view.html")
 @Push
-@PageTitle("bakery_12")
+@PageTitle("FullCalendar Demo")
 @Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes")
 public class MainView extends PolymerTemplate<TemplateModel> implements RouterLayout {
 
