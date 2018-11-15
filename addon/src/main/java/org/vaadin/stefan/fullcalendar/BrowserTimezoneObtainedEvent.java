e2b276e40f8f1014413b86bf08cb13e881d71d37
Stefan Uebe <stefan@vaadin.com>
2018-11-15 15:33:12 +0100

    added listener to be called when the browsers timezone has been obtained

---
+++ addon/src/main/java/org/vaadin/stefan/fullcalendar/BrowserTimezoneObtainedEvent.java
@@ -0,0 +1,30 @@
+package org.vaadin.stefan.fullcalendar;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.DomEvent;
+
+/**
+ * This event gets fired when the client side reported the browser's timezone to the server. Since this is
+ * done after the element has been attached to the client, it will be fired after all UI attach events.
+ */
+@DomEvent("browser-timezone-obtained")
+public class BrowserTimezoneObtainedEvent extends ComponentEvent<FullCalendar> {
+
+    private final Timezone timezone;
+
+    /**
+     * Creates a new event using the given source and indicator whether the
+     * event originated from the client side or the server side.
+     *
+     * @param source     the source component
+     * @param fromClient <code>true</code> if the event originated from the client
+     */
+    public BrowserTimezoneObtainedEvent(FullCalendar source, boolean fromClient, Timezone timezone) {
+        super(source, fromClient);
+        this.timezone = timezone;
+    }
+
+    public Timezone getTimezone() {
+        return timezone;
+    }
+}
