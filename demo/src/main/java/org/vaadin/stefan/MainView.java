df155059c39b974855234b7f5df5faef720e5c98
Stefan Uebe <stefan@vaadin.com>
2018-11-22 12:02:12 +0100

    corrected versions 2

--- demo/src/main/java/org/vaadin/stefan/MainView.java
+++ demo/src/main/java/org/vaadin/stefan/MainView.java
@@ -41,7 +41,7 @@ public class MainView extends PolymerTemplate<TemplateModel> implements RouterLa
     private HorizontalLayout title;
 
     public MainView() {
-        title.add(new H3("full calendar demo"), new Span("(FullCalendar addon: 1.7.11.2.1-SNAPSHOT, FullCalendar Scheduler extension: 1.2.11.2.1-SNAPSHOT)"));
+        title.add(new H3("full calendar demo"), new Span("(FullCalendar addon: 1.7.1, FullCalendar Scheduler extension: 1.2.1)"));
         title.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.BASELINE);
     }
 }
