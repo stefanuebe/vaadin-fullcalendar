f0aeb506de9a67f690f6675d9a2295413426e144
Stefan Uebe <stefan@vaadin.com>
2019-06-03 09:59:46 +0200

    optimized imports

--- demo/src/main/java/org/vaadin/stefan/MyFullCalendar.java
+++ demo/src/main/java/org/vaadin/stefan/MyFullCalendar.java
@@ -18,11 +18,7 @@ package org.vaadin.stefan;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import org.vaadin.stefan.fullcalendar.ClientSideValue;
 import org.vaadin.stefan.fullcalendar.FullCalendarScheduler;
-
-import javax.annotation.Nullable;
-import java.time.ZoneId;
 
 @Tag("my-full-calendar")
 @HtmlImport("frontend://my-full-calendar.html")
