421076c1a952ed2b2dbaa67bf6134cfd625e3b47
Stefan Uebe <stefan@vaadin.com>
2019-09-17 05:06:33 +0200

    new version, some minor internal changes

--- demo/src/main/java/org/vaadin/stefan/MainView.java
+++ demo/src/main/java/org/vaadin/stefan/MainView.java
@@ -16,8 +16,6 @@
  */
 package org.vaadin.stefan;
 
-import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -25,11 +23,8 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.page.Viewport;
-import com.vaadin.flow.component.polymertemplate.Id;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.RouterLayout;
-import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Push
 @PageTitle("FullCalendar Demo")
