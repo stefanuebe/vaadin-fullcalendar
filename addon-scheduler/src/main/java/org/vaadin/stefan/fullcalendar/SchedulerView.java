d054994a076ea5f61b81516597a16e8851c1d0f4
Stefan Uebe <stefan@vaadin.com>
2020-04-15 10:55:17 +0200

    updated API and comments

--- addon-scheduler/src/main/java/org/vaadin/stefan/fullcalendar/SchedulerView.java
+++ addon-scheduler/src/main/java/org/vaadin/stefan/fullcalendar/SchedulerView.java
@@ -20,17 +20,28 @@ package org.vaadin.stefan.fullcalendar;
  * Enumeration of possible scheduler views.
  */
 public enum SchedulerView implements CalendarView {
+
+    /**  Day timeline **/
     TIMELINE_DAY("timelineDay"),
+    /**  Week timeline **/
     TIMELINE_WEEK("timelineWeek"),
+    /**  Month timeline **/
     TIMELINE_MONTH("timelineMonth"),
+    /**  Year timeline **/
     TIMELINE_YEAR("timelineYear"),
 
+    /** Day timeline showing also resources **/
     RESOURCE_TIMELINE_DAY("resourceTimelineDay"),
+    /** Week timeline showing also resources **/
     RESOURCE_TIMELINE_WEEK("resourceTimelineWeek"),
+    /** Month timeline showing also resources **/
     RESOURCE_TIMELINE_MONTH("resourceTimelineMonth"),
+    /** Year timeline showing also resources **/
     RESOURCE_TIMELINE_YEAR("resourceTimelineYear"),
 
+    /** Day timegrid showing also resources */
     RESOURCE_TIME_GRID_DAY("resourceTimeGridDay"), // was AGENDA_DAY
+    /** Week timegrid showing also resources */
     RESOURCE_TIME_GRID_WEEK("resourceTimeGridWeek"), // was AGENDA_WEEK
 
     ;
