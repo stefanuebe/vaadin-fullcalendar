fb15c0fb5f21162970c2e48b77a6d82ae1ae1ef5
aetasoul <zanocco.carlo@gmail.com>
2021-06-07 17:21:43 +0200

    Fixed new demos

--- demo/src/main/java/org/vaadin/stefan/DemoCalendarWithBackgroundEvent.java
+++ demo/src/main/java/org/vaadin/stefan/DemoCalendarWithBackgroundEvent.java
@@ -1,32 +1,46 @@
 package org.vaadin.stefan;
 
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
 import org.vaadin.stefan.fullcalendar.*;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
+@Route(value = "demobackgroundevent", layout = MainView.class)
 public class DemoCalendarWithBackgroundEvent extends VerticalLayout {
     private static final long serialVersionUID = 1L;
+    
+    private FullCalendar calendar;
 
     public DemoCalendarWithBackgroundEvent() {
-        initBasicDemo();
+    	getStyle().set("flex-grow", "1");
+
+        createCalendarInstance();
+        addBackgroundEvent();
+
+        add(new H3("Calendar with background event"), calendar);
+        
+        setFlexGrow(1, calendar);
+        setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
     }
 
-    private void initBasicDemo() {
-        FullCalendar calendar = FullCalendarBuilder.create().withScheduler().build();
+    private void createCalendarInstance() {
+        calendar = FullCalendarBuilder.create().withScheduler().build();
         ((FullCalendarScheduler) calendar).setSchedulerLicenseKey("GPL-My-Project-Is-Open-Source");
         ((FullCalendarScheduler) calendar).setSlotWidth("150");
-        addBackgroundEvent(calendar);
+        
         calendar.setHeight(500);
         calendar.changeView(SchedulerView.TIMELINE_MONTH);
-        add(new H1("Calendar with background event"), calendar);
     }
 
-    private void addBackgroundEvent(FullCalendar calendar) {
+    private void addBackgroundEvent() {
         LocalDate now = LocalDate.now();
+        
         ResourceEntry entry = new ResourceEntry();
+        
         entry.setTitle("This special holiday");
         entry.setStart(now.withDayOfMonth(4).atStartOfDay(), calendar.getTimezone());
         entry.setEnd(entry.getStartUTC().plus(1, ChronoUnit.DAYS));
@@ -34,6 +48,7 @@ public class DemoCalendarWithBackgroundEvent extends VerticalLayout {
         entry.setColor("red");
         entry.setRenderingMode(Entry.RenderingMode.BACKGROUND);
         entry.setResourceEditable(true);
+        
         calendar.addEntry(entry);
     }
 
