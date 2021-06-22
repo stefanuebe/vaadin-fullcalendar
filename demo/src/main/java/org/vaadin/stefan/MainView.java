d9751f8148179f1887340d4425c5062f9f130870
Stefan Uebe <stefan@vaadin.com>
2021-06-22 13:54:47 +0200

    some minor changes

--- demo/src/main/java/org/vaadin/stefan/MainView.java
+++ demo/src/main/java/org/vaadin/stefan/MainView.java
@@ -40,9 +40,9 @@ public class MainView extends VerticalLayout implements RouterLayout {
 
         HorizontalLayout title = new HorizontalLayout();
 
-        String text = "<b>FullCalendar Demo</b> (Vaadin 14.6.3, FullCalendar addon: 3.0.0.rc1 " +
+        String text = "<b>FullCalendar Demo</b> (Vaadin 14.6.3, FullCalendar addon: 3.0.0 " +
                 "(uses FC "+ FullCalendar.FC_CLIENT_VERSION +"), " +
-                "FullCalendar Scheduler extension: 3.0.0.rc1 " +
+                "FullCalendar Scheduler extension: 3.0.0 " +
                 "(uses scheduler extension libs "+ FullCalendarScheduler.FC_SCHEDULER_CLIENT_VERSION +")";
 
         Span span = new Span();
