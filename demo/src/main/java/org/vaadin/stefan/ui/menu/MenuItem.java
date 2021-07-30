6fefee5d0c4dd1c75b42031d0b6296c7fbc80435
Stefan Uebe <stefan@vaadin.com>
2021-07-30 16:36:09 +0200

    Version updated

--- demo/src/main/java/org/vaadin/stefan/ui/menu/MenuItem.java
+++ demo/src/main/java/org/vaadin/stefan/ui/menu/MenuItem.java
@@ -3,8 +3,6 @@ package org.vaadin.stefan.ui.menu;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import com.vaadin.flow.component.icon.VaadinIcon;
-
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MenuItem {
 	String label();
