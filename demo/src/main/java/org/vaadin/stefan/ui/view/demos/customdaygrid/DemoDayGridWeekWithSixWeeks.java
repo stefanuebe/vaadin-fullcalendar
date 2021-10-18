e2f9fbc2bd99dced1f15315a11cde64c3f790ae3
aetasoul <zanocco.carlo@gmail.com>
2021-10-18 16:20:29 +0200

    add group id + demo for it

--- demo/src/main/java/org/vaadin/stefan/ui/view/demos/customdaygrid/DemoDayGridWeekWithSixWeeks.java
+++ demo/src/main/java/org/vaadin/stefan/ui/view/demos/customdaygrid/DemoDayGridWeekWithSixWeeks.java
@@ -4,7 +4,12 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
 import org.vaadin.stefan.fullcalendar.CalendarLocale;
+import org.vaadin.stefan.fullcalendar.Entry;
 import org.vaadin.stefan.fullcalendar.FullCalendar;
 import org.vaadin.stefan.fullcalendar.FullCalendarBuilder;
 import org.vaadin.stefan.fullcalendar.model.Header;
@@ -27,6 +32,8 @@ public class DemoDayGridWeekWithSixWeeks extends VerticalLayout {
     	createCalendarInstance();
     	
     	addAndExpand(calendar);
+    	
+    	addEntries();
     }
     
     private void initView() {
@@ -53,5 +60,34 @@ public class DemoDayGridWeekWithSixWeeks extends VerticalLayout {
         calendar.setHeaderToolbar(testHeader);
 
         calendar.changeView(calendarView);
+    }
+    
+    private void addEntries() {
+    	LocalDate now = LocalDate.now();
+        
+        Entry entry_1 = new Entry(UUID.randomUUID().toString());
+        
+        entry_1.setColor("lightgreen");
+        entry_1.setTitle("Entry 1 - testgroup");
+        entry_1.setStart(now.atStartOfDay());
+        entry_1.setEnd(now.plusDays(1).atTime(LocalTime.MAX));
+        entry_1.setGroupId("testgroup");
+        
+        Entry entry_2 = new Entry(UUID.randomUUID().toString());
+        
+        entry_2.setColor("lightgreen");
+        entry_2.setTitle("Entry 2");
+        entry_2.setStart(now.minusDays(3).atStartOfDay());
+        entry_2.setEnd(now.minusDays(2).atTime(LocalTime.MAX));
+        entry_2.setGroupId("testgroup");
+        
+        Entry entry_3 = new Entry(UUID.randomUUID().toString());
+        
+        entry_3.setColor("lightred");
+        entry_3.setTitle("Entry 3 - ungrouped");
+        entry_3.setStart(now.plusDays(2).atStartOfDay());
+        entry_3.setEnd(now.plusDays(5).atTime(LocalTime.MAX));
+        
+        calendar.addEntries(entry_1, entry_2, entry_3);
     }
 }
