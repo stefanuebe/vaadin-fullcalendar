3e38e99716c51b6765586cdc3166a0651e3ed86b
Stefan Uebe <stefan@vaadin.com>
2022-01-31 12:45:03 +0100

    Using Spring Boot for the demo instead of Jetty

---
+++ demo/src/main/java/org/vaadin/stefan/Application.java
@@ -0,0 +1,19 @@
+package org.vaadin.stefan;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
