da6a663c725a70ae0ab19bf8ad9f4e1b7085b44f
Stefan Uebe <stefan@vaadin.com>
2023-02-16 10:41:45 +0100

    Removed resource fetch, fixed initial setup of resources

--- addon-scheduler/src/main/resources/META-INF/resources/frontend/full-calendar/full-calendar-scheduler.ts
+++ addon-scheduler/src/main/resources/META-INF/resources/frontend/full-calendar/full-calendar-scheduler.ts
@@ -18,63 +18,109 @@
 */
 import {customElement} from "lit/decorators.js";
 
-import {FullCalendar} from "@vaadin/flow-frontend/full-calendar/full-calendar";
+import {FullCalendar, IterableObject} from "@vaadin/flow-frontend/full-calendar/full-calendar";
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
 
 @customElement("full-calendar-scheduler")
 export class FullCalendarScheduler extends FullCalendar {
 
+    // stores any options, that are set before the calendar is attached using "setOption"
+    private initialSchedulerOptions: IterableObject = {}
+
     protected createInitOptions(initialOptions: any) {
         const options = super.createInitOptions(initialOptions);
 
-        options.resources = (fetchInfo: any, successCallback: (p: any) => void, failureCallback: any) => {
-            (this as any).$server.fetchResourcesFromServer().then((array: any | any[]) => {
-                if (Array.isArray(array)) {
-                    successCallback(array);
-                } else {
-                    failureCallback("could not fetch");
-                }
-            })
-        };
+        options.resources = this.initialSchedulerOptions.resources ?? options.resources ?? [];
+
         options.plugins.push(resourceTimeGridPlugin, resourceTimelinePlugin);
 
         return options;
     }
 
-    refreshAllResources() {
-        this.calendar?.refetchEvents();
+    addResources(array: any[], scrollToLast: boolean) {
+        let calendar = this.calendar;
+        if (calendar) {
+            calendar.batchRendering(function () {
+                for (let i = 0; i < array.length; i++) {
+                    calendar!.addResource(array[i], scrollToLast);
+                }
+            });
+        } else {
+            if (!this.initialSchedulerOptions.resources) {
+                this.initialSchedulerOptions.resources = [];
+            }
+
+            this.initialSchedulerOptions.resources.push(...array);
+        }
+    }
+
+    removeResources(array: any[]) {
+        let calendar = this.calendar;
+        if (calendar) {
+            calendar.batchRendering(function () {
+                for (let i = 0; i < array.length; i++) {
+                    const resource = calendar!.getResourceById(array[i].id);
+                    if (resource != null) {
+                        resource.remove();
+                    }
+                }
+            });
+        } else {
+            console.warn("Removing specific resources before attaching is currently not supported. Please" +
+                " create a github issue at https://github.com/stefanuebe/vaadin_fullcalendar/issues," +
+                " if you need this function.")
+        }
+    }
+
+    removeAllResources() {
+        let calendar = this.calendar;
+        if (calendar) {
+            calendar.batchRendering(function () {
+                calendar!.getResources().forEach(r => r.remove());
+            });
+        } else {
+            delete this.initialSchedulerOptions.resources;
+        }
     }
 
     setResourceLabelClassNamesCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLabelClassNames', new Function("return " + s)());
     }
 
     setResourceLabelContentCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLabelContent', new Function("return " + s)());
     }
 
     setResourceLabelDidMountCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLabelDidMount', new Function("return " + s)());
     }
 
     setResourceLablelWillUnmountCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLabelWillUnmount', new Function("return " + s)());
     }
 
     setResourceLaneClassNamesCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLaneClassNames', new Function("return " + s)());
     }
 
     setResourceLaneContentCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLaneContent', new Function("return " + s)());
     }
 
     setResourceLaneDidMountCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLaneDidMount', new Function("return " + s)());
     }
 
     setResourceLaneWillUnmountCallback(s: string) {
+        // @ts-ignore
         this.calendar?.setOption('resourceLaneWillUnmount', new Function("return " + s)());
     }
 }
