53cffb942444b7579cf4d2de22f433aa4bf8a9b6
Stefan Uebe <stefan@vaadin.com>
2023-09-22 09:56:36 +0200

    Fix for https://github.com/stefanuebe/vaadin_fullcalendar/issues/171
    Removed the Constraint annotation, since the Payload type, that has to be returned for the payload method is either javax or jakarta based.

--- addon/src/main/java/org/vaadin/stefan/fullcalendar/NotNull.java
+++ addon/src/main/java/org/vaadin/stefan/fullcalendar/NotNull.java
@@ -10,14 +10,15 @@ import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Replacement for the NotNull annotation from javax, which has no real replacement in Jakarta
- * (Vaadin 24 / Spring Boot 3). This annotation is meant as a marker for any parameter or return value, that shall
+ * This annotation is meant as a marker for any parameter or return value, that shall
  * not be null at any point.
+ * <br><br>
+ * Replacement for the built in NotNull annotation from javax and jakarta to allow the usage of this lib in both
+ * environments.
  * @author Stefan Uebe
  */
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
 @Documented
-@Constraint(validatedBy = { })
 public @interface NotNull {
 }
