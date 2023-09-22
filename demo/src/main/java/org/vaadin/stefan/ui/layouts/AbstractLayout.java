401a8a178f875a459db8df0261852c6988a4e3d4
Stefan Uebe <stefan@vaadin.com>
2023-09-22 09:56:36 +0200

    Fix for https://github.com/stefanuebe/vaadin_fullcalendar/issues/171
    Removed the Constraint annotation, since the Payload type, that has to be returned for the payload method is either javax or jakarta based.

--- demo/src/main/java/org/vaadin/stefan/ui/layouts/AbstractLayout.java
+++ demo/src/main/java/org/vaadin/stefan/ui/layouts/AbstractLayout.java
@@ -42,7 +42,7 @@ import java.util.Locale;
 @CssImport("./app-layout-styles.css")
 @SuppressWarnings("rawtypes")
 public abstract class AbstractLayout extends AppLayout implements AfterNavigationObserver {
-    public static final String ADDON_VERSION = "6.0.0";
+    public static final String ADDON_VERSION = "6.0.1";
     private static final long serialVersionUID = -7479612679602267287L;
 
     @SuppressWarnings("unchecked")
