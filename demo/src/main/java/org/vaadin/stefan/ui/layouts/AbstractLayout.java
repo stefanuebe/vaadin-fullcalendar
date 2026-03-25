8433d72d1d522a7fa32a1094b455066beb58ef33
Stefan Uebe <stefan@vaadin.com>
2026-03-25 19:45:57 +0100

    7.1.5 release version

--- demo/src/main/java/org/vaadin/stefan/ui/layouts/AbstractLayout.java
+++ demo/src/main/java/org/vaadin/stefan/ui/layouts/AbstractLayout.java
@@ -46,7 +46,7 @@ import java.util.Locale;
 
 @SuppressWarnings("rawtypes")
 public abstract class AbstractLayout extends AppLayout implements AfterNavigationObserver {
-    public static final String ADDON_VERSION = "7.1.3-SNAPSHOT";
+    public static final String ADDON_VERSION = "7.1.5";
     private static final long serialVersionUID = -7479612679602267287L;
     private Registration currentStyleSheetRegistration;
     private Select<Theme> themeSelect;
